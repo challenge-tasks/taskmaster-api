7997839a4a4aeac61163ef9efd7ba555ccdb41bd
ramatov-zulfikor <ramatovv@gmail.com>
2023-07-23 12:51:47 +0500

    feat: Deploy command

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('deploy', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate');
+    \Illuminate\Support\Facades\Artisan::call('db:seed');
+    \Illuminate\Support\Facades\Artisan::call('db:seed FakeSeeder');
+});
