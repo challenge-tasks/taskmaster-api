52e47007559c52ebe91f102f1c93f4f54a57a703
ramatov-zulfikor <ramatovv@gmail.com>
2023-10-13 15:13:08 +0500

    feat: Add tags & stacks to task details API

--- app/Http/Resources/Api/V1/Task/TaskResource.php
+++ app/Http/Resources/Api/V1/Task/TaskResource.php
@@ -19,6 +19,20 @@ class TaskResource extends JsonResource
             'images' => $this->images && count($this->images) ? $this->images->pluck('image') : null,
             'difficulty' => $this->difficulty_label,
             'description' => $this->details?->description,
+            'stacks' => $this->stacks->map(function ($stack) {
+                return [
+                    'id' => $stack->id,
+                    'slug' => $stack->slug,
+                    'name' => $stack->name
+                ];
+            }),
+            'tags' => $this->tags->map(function ($tag) {
+                return [
+                    'id' => $tag->id,
+                    'slug' => $tag->slug,
+                    'name' => $tag->name
+                ];
+            }),
             'created_at' => strtotime($this->created_at),
             'updated_at' => strtotime($this->updated_at),
         ];
