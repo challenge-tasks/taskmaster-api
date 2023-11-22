47897509880df1c95e18bd1ea3456444b4dc9d84
ramatov-zulfikor <ramatovv@gmail.com>
2023-11-22 17:15:37 +0500

    chore: set tokens lifetime

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::tokensExpireIn(now()->addYear());
+        Passport::refreshTokensExpireIn(now()->addYear());
+        Passport::personalAccessTokensExpireIn(now()->addYear());
     }
 }
