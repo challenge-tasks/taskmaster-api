f8bfbb0dc8830463465137703577bede28944837
ramatov-zulfikor <ramatovv@gmail.com>
2023-12-03 12:06:45 +0500

    chore: update images quality constant

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -9,7 +9,7 @@ use Intervention\Image\Facades\Image;
 
 class ImageService
 {
-    const QUALITY = 85;
+    const QUALITY = 90;
 
     public function uploadAsWebp(UploadedFile $file, string $folder): string
     {
